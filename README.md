c65727d24db40ea90f02766e81ce6dac56dff837
mk-pmb <mk-pmb@users.noreply.github.com>
2016-11-27 02:06:44 +0100

    debug log

---
+++ README.md
@@ -0,0 +1,24 @@
+﻿
+<!--#echo json="package.json" key="name" underline="=" -->
+mini-lodash-v004-pmb
+====================
+<!--/#echo -->
+
+<!--#echo json="package.json" key="description" -->
+lodash v4.x, minified, on npm.
+<!--/#echo -->
+
+
+Usage
+-----
+:TODO:
+
+
+<!--#toc stop="scan" -->
+
+
+License
+-------
+<!--#echo json="package.json" key=".license" -->
+ISC
+<!--/#echo -->
---
+++ build/debug-lodash-cli.js
@@ -0,0 +1,58 @@
+﻿/*jslint indent: 2, maxlen: 80, continue: false, unparam: false, node: true */
+/* -*- tab-width: 2 -*- */
+'use strict';
+
+var nodeStart = Date.now(), yUNoExit = require('wtfnode').dump,
+  chProc = require('child_process'), origSpawn = chProc.spawn,
+  lodashCli, lostChildren = {};
+
+function logSpawn(cmd, args, opts) {
+  var child = origSpawn(cmd, args, opts), pid = child.pid;
+  cmd = [cmd].concat(args && args.slice().map(function (arg) {
+    return arg.replace(/^\/\S+(\/node_modules\/)/, '/…$1');
+  }));
+  lostChildren[pid] = cmd;
+  console.log({ child: pid, spawn: cmd });
+  child.on('exit', function (retval, signal) {
+    delete lostChildren[pid];
+    console.log({ despawn: pid, retval: retval, signal: signal, cmd: cmd });
+  });
+  ['in', 'out', 'err'].forEach(function (chan) {
+    var strm = child['std' + chan];
+    strm.on('end', function () { console.log({ child: pid, end: chan }); });
+  });
+  return child;
+}
+chProc.spawn = logSpawn;
+
+function runDura(what) {
+  var now = Date.now();
+  console.log('@', ((now - nodeStart) / 1000).toFixed(2), 'seconds:', what);
+}
+
+
+runDura('gonnaRequireLodashCli');
+lodashCli = require('lodash-cli');
+runDura('hasRequiredLodashCli');
+
+
+function scheduleForcedExit() {
+  runDura('scheduleForcedExit');
+  console.log('lostChildren:', lostChildren);
+  setTimeout(function () {
+    runDura('process.exit');
+    process.exit();
+  }, 500).unref();
+  yUNoExit();
+}
+
+
+runDura('invokeLodashCli');
+lodashCli([
+  'category=util',
+  'include=each',
+  'exports=amd,global',
+], function (result) {
+  console.log(result.source.split(/\*\/\s*\n/)[0]);
+  setTimeout(scheduleForcedExit, 10000).unref();
+});
